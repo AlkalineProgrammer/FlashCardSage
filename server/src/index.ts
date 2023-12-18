23c4107788b38e094dc37bccfe008f2127d97a5c
Abhishek_Diwakar <abhishek.diwakar@paymentz.com>
2023-12-18 16:33:56 +0530

    working on showing cards on landing page

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,22 @@
 import express, { Request, Response } from "express";
+import { config } from 'dotenv'
+config();
+import cors from 'cors'
 import mongoose from 'mongoose';
 import Deck from './models/Deck'
 
 const PORT = 5000;
 
 const app = express();
-
+app.use(cors())
 app.use(express.json());
 
+app.get('/decks', async (req: Request, res: Response) => {
+    const decks = await Deck.find();
+    res.json(decks);
+});
+
 app.post('/decks', async (req: Request, res: Response) => {
-    console.log(req.body)
     const newDeck = new Deck({
         title: req.body.title
     });
@@ -17,10 +24,7 @@ app.post('/decks', async (req: Request, res: Response) => {
     res.json(createdDeck);
 });
 
-mongoose.connect(
-    // 'mongodb://flashcardsage:@main-shard-00-00-03xkr.mongodb.net:27017,main-shard-00-01-03xkr.mongodb.net:27017,main-shard-00-02-03xkr.mongodb.net:27017/main?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true'
-    'mongodb+srv://flashcardsage:7seZeT3tlUBv3pBr@cluster0.6gfrb35.mongodb.net/?retryWrites=true&w=majority'
-).then(() => {
+mongoose.connect(process.env.MONGO_URL!).then(() => {
     console.log(`Listening to port ${PORT} `)
     app.listen(PORT);
 })
